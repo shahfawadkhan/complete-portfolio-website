4d5e81936963acaa960d998b2438ea0276033a4b
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-20 08:02:50 -0700

    update styling and add a hamburger menu

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-// document.querySelector("#change-bg").addEventListener('click', function(event) {
-//     event.preventDefault();  
-//     document.body.classList.toggle('alt-theme'); 
-// });
+
+let bar = document.getElementById("bar");
+let cross = document.getElementById("cross-mark");
+let navBar = document.getElementById("nav-links");
+
+// Function to handle opening and closing the menu
+function closeBar() {
+    bar.style.display = 'none';
+    cross.style.display = 'block';
+    navBar.style.display = 'flex';
+}
+
+function closeCross() {
+    bar.style.display = 'block';
+    cross.style.display = 'none';
+    navBar.style.display = 'none';
+}
+
+
+bar.addEventListener("click", closeBar);
+cross.addEventListener("click", closeCross);
+
+
+function checkScreenSize() {
+    if (window.innerWidth > 768) { 
+        bar.style.display = 'none';
+        cross.style.display = 'none';
+        navBar.style.display = 'flex'; 
+    } else {
+      
+        bar.style.display = 'block';
+        cross.style.display = 'none';
+        navBar.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', checkScreenSize);
+
